455873404d98734770c6333eb68643e9deebdf70
Erfan Khaksar <erfan1375@gmail.com>
2020-04-12 09:42:17 -0400

    Initial URLs

---
+++ linked/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from . import views
+# localhost:8000/linked/2
+urlpatterns = [
+    path('', views.index, name='index'),                                    # Home page
+    path('login/', views.login, name='login'),                              # Login page
+    path('signup/', views.signup, name='signup'),                           # Registration page
+    path('logout/', views.logout, name='logout'),                           # Logout endpoint
+    path('profile/', views.profile, name='profile'),                        # Own profile page
+    path('new/', views.new, name='new'),                                    # Own profile page
+    path('profile/<str:username>/', views.profile, name='profile'),         # Other profile page
+    path('<int:link_id>/', views.detail, name="detail"),                    # Link detail
+    path('redirect/<int:link_id>/', views.redirect, name="redirect"),       # Redirection
+]
+
